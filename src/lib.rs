457a1fb7a59aa11f049f35e1ddcba0e1f97da35e
Martin Stancsics <martin.stancsics@gmail.com>
2020-12-31 10:58:39 +0100

    Add function to analyze a folder

--- src/lib.rs
+++ src/lib.rs
@@ -10,6 +10,7 @@ pub struct AnalyzedFile {
     ext: Option<String>,
     raw_creation_time: Option<time::SystemTime>,
     creation_time: Option<DateTime<Utc>>,
+    to_copy: Option<bool>,
 }
 
 impl AnalyzedFile {
@@ -26,7 +27,7 @@ impl AnalyzedFile {
         let raw_creation_time = file
             .metadata().ok()
             .and_then(|md| md.created().ok());
-        let creation_time: Option<DateTime<Utc>> = raw_creation_time
+        let creation_time: Option<DateTime<Utc>> = raw_creation_time // TODO: time zones?
             .map(|ct| DateTime::from(ct));
 
         AnalyzedFile {
@@ -35,6 +36,21 @@ impl AnalyzedFile {
             ext,
             raw_creation_time,
             creation_time,
+            to_copy: None,
         }
     }
+}
+
+pub fn analyze_dir(dir: &str) -> io::Result<Vec<AnalyzedFile>> {
+    let entries = fs::read_dir(dir)?;
+    Ok(
+        entries
+            .filter_map(Result::ok)
+            .filter(|f| match f.file_type() {
+                Ok(ft) => ft.is_file(),  // TODO: what to do about symlinks?
+                Err(_) => false
+            })
+            .map(AnalyzedFile::from_direntry)
+            .collect()
+    )
 }
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,16 @@
-use std::fs::DirEntry;
-
 use clap::{App, load_yaml};
-use import_pics::AnalyzedFile;
+use import_pics::analyze_dir;
 
 fn main() {
     let yaml = load_yaml!("cli.yaml");
     let matches = App::from(yaml).get_matches();
 
-    let from = matches.value_of("FROM").expect("Source folder must be specified");
-    let to = matches.value_of("TO").expect("Destination folder must be specified");
+    let _from = matches.value_of("FROM").expect("Source folder must be specified");
+    let _to = matches.value_of("TO").expect("Destination folder must be specified");
 
     // DEBUG:
-    for entry in std::fs::read_dir(r"C:\Users\mstancs\Pictures\Screenshots").unwrap() {
-        let entry = entry.unwrap();
-        println!("{:?}", AnalyzedFile::from_direntry(entry));
-    }
+    println!(
+        "{:?}",
+        analyze_dir(r"C:\Users\mstancs\Pictures\Screenshots").unwrap()
+    );
 }
