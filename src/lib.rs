ad76681d391451b1ec32f5cca3ad43f33156cc15
Martin Stancsics <martin.stancsics@gmail.com>
2020-12-31 13:07:48 +0100

    Format with rustfmt

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,8 @@
-use std::fs;
-use std::time;
-use std::io;
 use chrono::{DateTime, Utc};
+use std::fs;
+use std::io;
 use std::path::PathBuf;
+use std::time;
 
 #[derive(Debug)]
 pub struct AnalyzedFile {
@@ -16,20 +16,19 @@ pub struct AnalyzedFile {
 
 impl AnalyzedFile {
     pub fn from_direntry(file: fs::DirEntry) -> Self {
-
         let path = file.path();
-        let stem = path.file_stem()
+        let stem = path
+            .file_stem()
             .and_then(|s| s.to_str())
             .map(|s| s.to_string())
             .unwrap_or("".to_string());
-        let ext = path.extension()
+        let ext = path
+            .extension()
             .and_then(|s| s.to_str())
             .map(|s| s.to_string().to_lowercase())
             .unwrap_or("".to_string());
 
-        let raw_creation_time = file
-            .metadata().ok()
-            .and_then(|md| md.created().ok());
+        let raw_creation_time = file.metadata().ok().and_then(|md| md.created().ok());
         let creation_time: Option<DateTime<Utc>> = raw_creation_time // TODO: time zones?
             .map(|ct| DateTime::from(ct));
 
@@ -44,14 +43,17 @@ impl AnalyzedFile {
     }
 }
 
-pub fn analyze_dir(dir: PathBuf, extensions: Option<Vec<&str>>, recursive: bool) -> io::Result<Vec<AnalyzedFile>> {
+pub fn analyze_dir(
+    dir: PathBuf,
+    extensions: Option<Vec<&str>>,
+    recursive: bool,
+) -> io::Result<Vec<AnalyzedFile>> {
     let extensions = match extensions {
         Some(ext) => ext,
-        None => vec!["jpg", "jpeg", "tif", "tiff", "raw", "arw", "mp4"]
+        None => vec!["jpg", "jpeg", "tif", "tiff", "raw", "arw", "mp4"],
     };
 
-    let entries = fs::read_dir(dir)?
-        .filter_map(Result::ok);
+    let entries = fs::read_dir(dir)?.filter_map(Result::ok);
 
     let mut files = Vec::new();
     let mut dirs = Vec::new();
@@ -64,30 +66,31 @@ pub fn analyze_dir(dir: PathBuf, extensions: Option<Vec<&str>>, recursive: bool)
                 } else if ft.is_dir() {
                     dirs.push(entry)
                 }
-            },
+            }
             Err(_) => continue, // Should there be some message here?
         }
     }
-    
-    let mut analyzed_files: Vec<AnalyzedFile> = files.into_iter()
-            .filter(|f| match f.file_type() {
-                Ok(ft) => ft.is_file(),  // TODO: what to do about symlinks?
-                Err(_) => false
-            })
-            .map(AnalyzedFile::from_direntry)
-            .filter(|af| extensions.contains(&af.ext.as_str()))
-            .collect();
+
+    let mut analyzed_files: Vec<AnalyzedFile> = files
+        .into_iter()
+        .filter(|f| match f.file_type() {
+            Ok(ft) => ft.is_file(), // TODO: what to do about symlinks?
+            Err(_) => false,
+        })
+        .map(AnalyzedFile::from_direntry)
+        .filter(|af| extensions.contains(&af.ext.as_str()))
+        .collect();
 
     if recursive {
-        for analyzed_file in dirs.into_iter() 
-            .flat_map(|dir| match analyze_dir(dir.path(), Some(extensions.clone()), recursive) {
+        for analyzed_file in dirs.into_iter().flat_map(|dir| {
+            match analyze_dir(dir.path(), Some(extensions.clone()), recursive) {
                 Ok(sub_files) => sub_files,
                 Err(_) => vec![],
-            }) {
-                analyzed_files.push(analyzed_file)
             }
+        }) {
+            analyzed_files.push(analyzed_file)
+        }
     }
-        
-    Ok(analyzed_files)
 
+    Ok(analyzed_files)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,20 @@
-use std::path::PathBuf;
-use clap::{App, load_yaml};
+use clap::{load_yaml, App};
 use import_pics::analyze_dir;
+use std::path::PathBuf;
 
 fn main() {
     let yaml = load_yaml!("cli.yaml");
     let matches = App::from(yaml).get_matches();
 
-    let _from = matches.value_of("FROM").expect("Source folder must be specified");
-    let _to = matches.value_of("TO").expect("Destination folder must be specified");
-    let recursive =  matches.is_present("recursive");
+    let _from = matches
+        .value_of("FROM")
+        .expect("Source folder must be specified");
+    let _to = matches
+        .value_of("TO")
+        .expect("Destination folder must be specified");
+    let recursive = matches.is_present("recursive");
 
     // DEBUG:
     let path = PathBuf::from(r"C:\Users\mstancs\Pictures\Screenshots");
-    println!(
-        "{:?}",
-        analyze_dir(path, None, recursive).unwrap()
-    );
+    println!("{:?}", analyze_dir(path, None, recursive).unwrap());
 }
